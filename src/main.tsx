07e6eee3b5dc2bf71565ea3b8b348d650f01fcfc
Raj Rathore <parth.baldaniya@sculptsoft.com>
2025-01-22 17:40:17 +0530

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import Providers from "./components/core/providers.tsx";
+import Sidebar from "./components/layout/sidebar.tsx";
+import { Toaster } from "./components/ui/toaster.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Providers>
+        <Toaster />
+        <Sidebar />
+        <main className="w-full px-3">
+          <App />
+        </main>
+      </Providers>
+    </BrowserRouter>
+  </StrictMode>,
+);
