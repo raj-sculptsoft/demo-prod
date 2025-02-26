70eea74149b337e25191a24e36229e7f030ac385
raj-sculptsoft <raj.rathore@sculptsoft.com>
2025-02-26 18:21:16 +0530

    Add env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,6 @@
 const config = {
   COMPANY_ID: "b85a7561-6383-446a-aa5b-5cb3b5679e38", // Unique identifier for the company (used in API requests)
-  VITE_PUBLIC_API_URL: "https://app.dev.prdsec.com", // Base URL for API requests
+  VITE_PUBLIC_API_URL: import.meta.env.VITE_PUBLIC_API_URL, // Base URL for API requests
 };
 
 export default config;
